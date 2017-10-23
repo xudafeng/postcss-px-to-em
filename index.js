e6d2870ffb220daf49176f705e50b5c7eed63613
xudafeng <xudafeng@126.com>
2017-10-23 21:57:37 +0800

    typo

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var DEFAULTS = {
   base: 16,
 };
 
-function nonForcedNumericRegex(number) {
+var nonForcedNumericRegex = function(number) {
   // finds pixel values not followed by `/* force */`
   return new RegExp(number + 'px(?!\\s*\\/\\*\\s*force\\s*\\*\\/)', 'g');
-}
+};
 
-module.exports = postcss.plugin('postcss-px-2-em', function (opts) {
+module.exports = postcss.plugin('postcss-px-2-em', function(opts) {
   opts = extend({}, DEFAULTS, opts);
 
   var regex = /([\d\.]+)px(\s*\/\*\s*force\s*\*\/)?/g;
@@ -25,7 +25,7 @@ module.exports = postcss.plugin('postcss-px-2-em', function (opts) {
 
         // if the value is not forced to be pixels, let's replace any matching
         if (!matches[2]) {
-          context = context.replace(nonForcedNumericRegex(matches[1]), matches[1] / opts.base + 'em');
+          context = context.replace(nonForcedNumericRegex(matches[1]), `${matches[1] / opts.base}em`);
         }
       });
     }
@@ -33,7 +33,7 @@ module.exports = postcss.plugin('postcss-px-2-em', function (opts) {
     return context;
   };
 
-  return function (css) {
+  return function(css) {
     css.walkDecls(function(node) {
       node.value = convert(node.raws.value ? node.raws.value.raw : node.value);
     });
